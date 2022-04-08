24f06970c52002ac02483f961143ec824d5716b1
Lolo <lorenzo.lentini-pro@outlook.fr>
2022-04-08 13:48:53 +0200

    Created method attempting to fetch raw data from json files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-waste-manager';
+
+  products: any = [];
+
+  constructor(private httpClient: HttpClient){}
+  ngOnInit(){
+    this.httpClient.get("assets/data.json").subscribe(data =>{
+      console.log(data);
+      this.products = data;
+    })
+  }
 }
